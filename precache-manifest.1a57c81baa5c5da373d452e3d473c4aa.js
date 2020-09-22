3607d88f47e7943f05e481ad3960ba22578b6ae4
nomiso2000 <nomiso.study@gmail.com>
2020-09-22 23:09:21 +0300

    Updates

---
+++ precache-manifest.1a57c81baa5c5da373d452e3d473c4aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c3238b21df9426601db647b7c997c1",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "89e15c0d3f9926a4f177",
+    "url": "/goit-react-hw-02-feedback/static/css/main.ae098e8f.chunk.css"
+  },
+  {
+    "revision": "999dac2a3dd84adf7df9",
+    "url": "/goit-react-hw-02-feedback/static/js/2.5036897f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.5036897f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e15c0d3f9926a4f177",
+    "url": "/goit-react-hw-02-feedback/static/js/main.4f996f75.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
